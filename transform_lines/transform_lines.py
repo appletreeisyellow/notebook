0c40ee89f1ebb178e152eb368cee833d5ef040bb
Chunchun <14298407+appletreeisyellow@users.noreply.github.com>
2023-02-23 13:40:09 -0600

    chore: add function to add quotation before comments

--- transform_lines/transform_lines.py
+++ transform_lines/transform_lines.py
@@ -2,8 +2,24 @@
 input_file = open("list.txt", 'r')
 input_content = input_file.readlines()
 
+
+def add_quotation(string):
+    # e.g. `SELECT` --> `'SELECT',`
+    print("\'{}\',".format(row.strip()))
+
+
+def add_quotation_before_comment(string):
+    # e.g. `ADD   // +` --> `'ADD', // +`
+    line = row.strip().split('//')
+    if len(line) > 1:
+        print("\'{}\', //{}".format(line[0].strip(), line[1]))
+
+
+# Main block
 for row in input_content:
-    print("\'{}\',".format(row.strip()))  # e.g. SELECT --> 'SELECT',
+    # add_quotation(row)
+    add_quotation_before_comment(row)
+
 
 # Close the input file
 input_file.close()
